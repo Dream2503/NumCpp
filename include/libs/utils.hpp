94b32e5a430c4e412d4d5ca379cd8f621637ba86
Dream2503 <swapnarajmohanty2503.com>
2025-08-05 01:06:44 +0530

    [about to change] conflicting std::complex operations for proper initialization of array factory function

--- include/libs/utils.hpp
+++ include/libs/utils.hpp
@@ -3,23 +3,23 @@
 
 namespace numcpp {
     template <typename V>
-    array<V> arange(const range<V>&);
-    inline array<double> ones(const shape_t&);
-    template <typename V>
-    array<V> fill(const shape_t&, const V&);
-    inline array<double> zeros(const shape_t&);
-} // namespace numcpp
-
-namespace numcpp {
-    template <typename V>
-    array<V> arange(const range<V>& range) {
-        return array(std::vector<V>(range.begin(), range.end()), 1, range.size());
+    array<V> arange(const range_t<V>& range) {
+        return array(std::vector<V>(range.begin(), range.end()), {1, range.size()});
     }
 
-    array<double> ones(const shape_t& shape) { return fill(shape, 1.0); }
+    template <typename V>
+    array<V> fill(const shape_t& shape, const V& value) {
+        std::vector<V> res(shape.rows * shape.cols, value);
+        return array(std::move(res), shape);
+    }
+    template <typename V = double>
+    array<V> ones(const shape_t& shape) { return fill(shape, V() + 1); }
 
     template <typename V>
-    array<V> fill(const shape_t& shape, const V& value) {}
-
-    array<double> zeros(const shape_t& shape) { return fill(shape, 0.0); }
+    array<V> empty(const shape_t& shape) {
+        buffer_t<V> data(shape.rows * shape.cols);
+        return array<V>(std::move(data), shape, false);
+    }
+    template <typename V = double>
+    array<V> zeros(const shape_t& shape) { return fill(shape, V()); }
 } // namespace numcpp
