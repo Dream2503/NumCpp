c34350fa0e5de80f8f86a5d2da2cb5e972587a79
Dream2503 <swapnarajmohanty2503.com>
2025-08-06 01:35:01 +0530

    -> added a fully qualified complex_t to support various functionalities that std::complex doesn't -> added type alias as dtype namespace (doesn't seem useful, may be removed afterward soon) -> added a type trait to check extract real type from complex type if exists

--- include/libs/traits.hpp
+++ include/libs/traits.hpp
@@ -1,19 +1,23 @@
 #pragma once
 
+template <typename V>
+std::string format(const V&);
+
 namespace numcpp {
     template <typename>
     class array;
 
     template <typename V = int>
     struct none_t {
-        explicit constexpr operator V() const { return std::numeric_limits<V>::max(); }
+        constexpr operator V() const { return std::numeric_limits<V>::max(); }
     };
 
-    struct axis_t {
+    class axis_t {
         enum class type_t { none, single, pair } type;
         int axis0, axis1;
 
-        axis_t() noexcept : type(type_t::none), axis0(-1), axis1(-1) {}
+    public:
+        axis_t() noexcept : axis_t(none_t()) {}
         axis_t(none_t<>) noexcept : type(type_t::none), axis0(-1), axis1(-1) {}
         axis_t(const int axis) noexcept : type(type_t::single), axis0(axis), axis1(-1) {}
         axis_t(const int i, const int j) noexcept : type(type_t::pair), axis0(i), axis1(j) {}
@@ -30,9 +34,10 @@ namespace numcpp {
     template <typename V>
     class buffer_t {
         std::shared_ptr<V[]> value;
-        size_t size = 0;
 
     public:
+        size_t size = 0;
+
         buffer_t() noexcept = default;
         buffer_t(const buffer_t&) noexcept = default;
         buffer_t(buffer_t&&) noexcept = default;
@@ -41,7 +46,7 @@ namespace numcpp {
         buffer_t(std::shared_ptr<V[]> ptr, const size_t n) noexcept : value(std::move(ptr)), size(n) {}
         buffer_t(V* raw_ptr, const size_t n, std::nullptr_t) noexcept : value(raw_ptr, [](V*) {}), size(n) {}
 
-        buffer_t(std::vector<V>&& vec) : size(vec.size()) {
+        buffer_t(std::vector<V>&& vec) noexcept : size(vec.size()) {
             V* raw = vec.data();
             value = std::shared_ptr<V[]>(raw, [vec = std::move(vec)](V*) mutable {});
         }
@@ -50,7 +55,7 @@ namespace numcpp {
         buffer_t& operator=(buffer_t&&) noexcept = default;
         V& operator[](const size_t i) noexcept { return value[i]; }
         const V& operator[](const size_t i) const noexcept { return value[i]; }
-        explicit operator bool() const noexcept { return static_cast<bool>(value); }
+        operator bool() const noexcept { return static_cast<bool>(value); }
 
         V* data() noexcept { return value.get(); }
         const V* data() const noexcept { return value.get(); }
@@ -60,11 +65,137 @@ namespace numcpp {
             size = 0;
         }
 
-        void reset(const V* ptr, const size_t n) {
+        void reset(const V* ptr, const size_t n) noexcept {
             value.reset(ptr);
             size = n;
         }
     };
+
+    template <typename V>
+    struct complex_t {
+        using value_type = V;
+        V real, imag;
+
+        constexpr complex_t() noexcept : complex_t(0, 0) {}
+        constexpr complex_t(const V real) noexcept : complex_t(real, 0) {}
+        constexpr complex_t(const V real, const V imag) noexcept : real(real), imag(imag) {}
+
+        constexpr complex_t operator+() const noexcept { return *this; }
+        constexpr complex_t operator-() const noexcept { return complex_t(-real, -imag); }
+
+        constexpr complex_t operator+(const complex_t& other) const noexcept {
+            return complex_t(real + other.real, imag + other.imag);
+        }
+        constexpr complex_t operator-(const complex_t& other) const noexcept {
+            return complex_t(real - other.real, imag - other.imag);
+        }
+        constexpr complex_t operator*(const complex_t& other) const noexcept {
+            return complex_t(real * other.real - imag * other.imag, real * other.imag + imag * other.real);
+        }
+        constexpr complex_t operator/(const complex_t& other) const noexcept {
+            V denominator = other.real * other.real + other.imag * other.imag;
+            return complex_t((real * other.real + imag * other.imag) / denominator,
+                             (imag * other.real - real * other.imag) / denominator);
+        }
+
+        constexpr complex_t operator+(const V value) const noexcept { return complex_t(real + value, imag); }
+        constexpr complex_t operator-(const V value) const noexcept { return complex_t(real - value, imag); }
+        constexpr complex_t operator*(const V value) const noexcept { return complex_t(real * value, imag * value); }
+        constexpr complex_t operator/(const V value) const noexcept { return complex_t(real / value, imag / value); }
+
+        complex_t& operator+=(const complex_t& other) noexcept {
+            real += other.real;
+            imag += other.imag;
+            return *this;
+        }
+        complex_t& operator-=(const complex_t& other) noexcept {
+            real -= other.real;
+            imag -= other.imag;
+            return *this;
+        }
+        complex_t& operator*=(const complex_t& other) noexcept {
+            V r = real * other.real - imag * other.imag, i = real * other.imag + imag * other.real;
+            real = r;
+            imag = i;
+            return *this;
+        }
+        complex_t& operator/=(const complex_t& other) noexcept {
+            V denominator = other.real * other.real + other.imag * other.imag;
+            V r = (real * other.real + imag * other.imag) / denominator;
+            V i = (imag * other.real - real * other.imag) / denominator;
+            real = r;
+            imag = i;
+            return *this;
+        }
+
+        complex_t& operator+=(const V value) noexcept {
+            real += value;
+            return *this;
+        }
+        complex_t& operator-=(const V value) noexcept {
+            real -= value;
+            return *this;
+        }
+        complex_t& operator*=(V value) noexcept {
+            real *= value;
+            imag *= value;
+            return *this;
+        }
+        complex_t& operator/=(V value) noexcept {
+            real /= value;
+            imag /= value;
+            return *this;
+        }
+
+        constexpr bool operator==(const complex_t& other) const noexcept {
+            return real == other.real && imag == other.imag;
+        }
+        constexpr bool operator!=(const complex_t& other) const noexcept { return !(*this == other); }
+        constexpr operator bool() const noexcept { return real != 0 || imag != 0; }
+
+        V abs() const noexcept { return std::sqrt(norm()); }
+        constexpr complex_t conj() const noexcept { return complex_t(real, -imag); }
+        constexpr V norm() const noexcept { return real * real + imag * imag; }
+        std::complex<V> to_std() const noexcept { return std::complex<V>(real, imag); }
+
+        friend std::ostream& operator<<(std::ostream& out, const complex_t& complex) noexcept {
+            if (!complex.imag) {
+                out << format(complex.real);
+            } else if (!complex.real) {
+                out << format(complex.imag) << "j";
+            } else {
+                out << format(complex.real) << (complex.imag < 0 ? "-" : "+") << format(std::abs(complex.imag)) << "j";
+            }
+            return out;
+        }
+
+        static complex_t from_polar(V magnitude, V angle_rad) noexcept {
+            return complex_t(magnitude * std::cos(angle_rad), magnitude * std::sin(angle_rad));
+        }
+    };
+
+    namespace dtype {
+        using bool_t = bool;
+        using int8_t = int8_t;
+        using int16_t = int16_t;
+        using int32_t = int32_t;
+        using int64_t = int64_t;
+        using int128_t = __int128_t;
+
+        using uint8_t = uint8_t;
+        using uint16_t = uint16_t;
+        using uint32_t = uint32_t;
+        using uint64_t = uint64_t;
+        using uint128_t = __uint128_t;
+
+        using float32_t = float;
+        using float64_t = double;
+        using float128_t = long double;
+
+        using complex64_t = complex_t<float>;
+        using complex128_t = complex_t<double>;
+        using complex256_t = complex_t<long double>;
+    } // namespace dtype
 
     using ll_t = long long;
 
@@ -72,7 +203,7 @@ namespace numcpp {
         bool resolved = false;
 
     public:
-        static constexpr ll_t none = static_cast<ll_t>(none_t<ll_t>());
+        static constexpr ll_t none = none_t<ll_t>();
         ll_t start, stop, step;
 
         explicit slice_t(const ll_t start = none, const ll_t stop = none, const ll_t step = 1) :
@@ -112,48 +243,48 @@ namespace numcpp {
     };
 
     struct index_t {
-        std::variant<ll_t, slice_t> row, col;
+        const std::variant<ll_t, slice_t> row, col;
 
-        index_t(ll_t i, ll_t j) : row(i), col(j) {}
-        index_t(ll_t i, const slice_t& j) : row(i), col(j) {}
-        index_t(const slice_t& i, ll_t j) : row(i), col(j) {}
-        index_t(const slice_t& i, const slice_t& j) : row(i), col(j) {}
+        index_t(const ll_t i, const ll_t j) noexcept : row(i), col(j) {}
+        index_t(const ll_t i, const slice_t& j) noexcept : row(i), col(j) {}
+        index_t(const slice_t& i, const ll_t j) noexcept : row(i), col(j) {}
+        index_t(const slice_t& i, const slice_t& j) noexcept : row(i), col(j) {}
 
-        bool is_scalar_row() const { return std::holds_alternative<ll_t>(row); }
-        bool is_scalar_col() const { return std::holds_alternative<ll_t>(col); }
-        bool is_scalar() const { return is_scalar_row() && is_scalar_col(); }
-        bool is_slice_row() const { return std::holds_alternative<slice_t>(row); }
-        bool is_slice_col() const { return std::holds_alternative<slice_t>(col); }
-        bool is_slice() const { return is_slice_row() && is_slice_col(); }
+        bool is_scalar_row() const noexcept { return std::holds_alternative<ll_t>(row); }
+        bool is_scalar_col() const noexcept { return std::holds_alternative<ll_t>(col); }
+        bool is_scalar() const noexcept { return is_scalar_row() && is_scalar_col(); }
+        bool is_slice_row() const noexcept { return std::holds_alternative<slice_t>(row); }
+        bool is_slice_col() const noexcept { return std::holds_alternative<slice_t>(col); }
+        bool is_slice() const noexcept { return is_slice_row() && is_slice_col(); }
 
-        ll_t get_scalar_row() const { return std::get<ll_t>(row); }
-        ll_t get_scalar_col() const { return std::get<ll_t>(col); }
-        std::pair<ll_t, ll_t> get_scalar() const { return {get_scalar_row(), get_scalar_col()}; }
-        const slice_t& get_slice_row() const { return std::get<slice_t>(row); }
-        const slice_t& get_slice_col() const { return std::get<slice_t>(col); }
-        std::pair<slice_t, slice_t> get_slice() const { return {get_slice_row(), get_slice_col()}; }
+        ll_t get_scalar_row() const noexcept { return std::get<ll_t>(row); }
+        ll_t get_scalar_col() const noexcept { return std::get<ll_t>(col); }
+        std::pair<ll_t, ll_t> get_scalar() const noexcept { return {get_scalar_row(), get_scalar_col()}; }
+        const slice_t& get_slice_row() const noexcept { return std::get<slice_t>(row); }
+        const slice_t& get_slice_col() const noexcept { return std::get<slice_t>(col); }
+        std::pair<slice_t, slice_t> get_slice() const noexcept { return {get_slice_row(), get_slice_col()}; }
     };
 
     template <typename>
     struct is_complex : std::false_type {};
 
     template <typename V>
-    struct is_complex<std::complex<V>> : std::true_type {};
+    struct is_complex<complex_t<V>> : std::true_type {};
 
     template <typename V>
     constexpr bool is_complex_v = is_complex<V>::value;
 
+    template <typename V>
     struct out_t {
-        array<bool>* ptr;
+        array<V>* ptr;
 
-        out_t() noexcept = default;
-        explicit out_t(array<bool>& arr) noexcept : ptr(&arr) {}
+        out_t() noexcept : ptr(nullptr) {}
+        out_t(array<V>& arr) noexcept : ptr(&arr) {}
         out_t(std::nullptr_t) noexcept : ptr(nullptr) {}
 
         constexpr operator bool() const noexcept { return ptr != nullptr; }
-
-        bool is_set() const noexcept { return *this; }
-        array<bool>& get() const noexcept { return *ptr; }
+        constexpr array<V>& operator*() { return *ptr; }
+        constexpr array<V>* operator->() { return ptr; }
     };
 
     template <typename L, typename R>
@@ -177,12 +308,12 @@ namespace numcpp {
             bool operator==(const iterator& other) const noexcept { return current == other.current; }
             bool operator!=(const iterator& other) const noexcept { return !(*this == other); }
 
-            iterator& operator++() {
+            iterator& operator++() noexcept {
                 current += step;
                 return *this;
             }
 
-            iterator operator++(int) {
+            iterator operator++(int) noexcept {
                 iterator tmp = *this;
                 ++(*this);
                 return tmp;
@@ -193,7 +324,7 @@ namespace numcpp {
 
         range_t(const V stop) noexcept : range_t(0, stop) {}
 
-        range_t(const V start, const V stop, const V step = 1) : start(start), stop(stop), step(step) {
+        range_t(const V start, const V stop, const V step = 1) noexcept : start(start), stop(stop), step(step) {
             if (step == V(0)) {
                 throw std::invalid_argument("range step cannot be zero");
             }
@@ -213,6 +344,19 @@ namespace numcpp {
         iterator end() const noexcept { return iterator(start + step * static_cast<V>(size()), stop, step); }
     };
 
+    template <typename V, bool = is_complex_v<V>>
+    struct real_type {
+        using type = V;
+    };
+
+    template <typename V>
+    struct real_type<V, true> {
+        using type = typename V::value_type;
+    };
+
+    template <typename V>
+    using real_t = typename real_type<V>::type;
+
     struct shape_t {
         size_t rows, cols;
 
@@ -224,13 +368,12 @@ namespace numcpp {
     struct where_t {
         const array<bool>* ptr;
 
-        where_t() noexcept = default;
-        explicit where_t(const array<bool>& arr) noexcept : ptr(&arr) {}
+        where_t() noexcept : ptr(nullptr) {}
+        where_t(const array<bool>& arr) noexcept : ptr(&arr) {}
         where_t(std::nullptr_t) noexcept : ptr(nullptr) {}
 
         constexpr operator bool() const noexcept { return ptr != nullptr; }
-
-        bool is_set() const noexcept { return *this; }
-        const array<bool>& get() const noexcept { return *ptr; }
+        constexpr const array<bool>& operator*() const noexcept { return *ptr; }
+        constexpr const array<bool>* operator->() const noexcept { return ptr; }
     };
 } // namespace numcpp
