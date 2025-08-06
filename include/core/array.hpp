60b0ad93c0aba298c6fb831210cb9e3c68c55e0b
Dream2503 <swapnarajmohanty2503.com>
2025-08-07 01:35:12 +0530

    -> renamed data -> buffer -> added MaskedArray as friend qualified class internal class with array<uint_8> storage type -> minor constructor changes -> added a getter for buffer [data()] -> added new operation [abs()] -> added minor overloads for abs(), real(), imag() -> subtle bug fix at operator= of type array& in else if (this != &other) previous == -> added implicit convertion from array<bool_t> to bitref_t ot bool_t only if scalar -> again minor change in operator<< to support bool_t output (surely no more operator<< changes)

--- include/core/array.hpp
+++ include/core/array.hpp
@@ -4,56 +4,51 @@
 
 template <typename V>
 class numcpp::array {
-    buffer_t<V> data;
+    buffer_t<V> buffer;
     size_t row, col, offset, row_stride, col_stride;
     const void* _base;
     bool is_matrix, is_scalar, is_assignable;
 
     template <typename>
     friend class array;
+    friend class MaskedArray;
 
-    void flat_constructor(auto begin, auto end, shape_t shape) {
+    array flat_constructor(auto begin, auto end, shape_t shape) {
         if (shape.cols == none::size) {
             shape.cols = end - begin;
         }
         if (shape.rows * shape.cols != end - begin) {
             throw std::invalid_argument("Size mismatch in flat constructor");
         }
-        row = shape.rows;
-        col = shape.cols;
-        offset = 0;
-        row_stride = col;
-        col_stride = 1;
-        _base = none::base;
-        is_matrix = row > 1 && col > 1;
-        is_scalar = is_assignable = false;
-        data = buffer_t<V>(row * col);
-        std::copy(begin, end, data.data());
+        buffer = buffer_t<V>(shape.rows * shape.cols);
+        std::copy(begin, end, buffer.data());
+        return array(std::move(buffer), shape, 0, shape.cols, 1, none::base, shape.rows > 1 && shape.cols > 1, false,
+                     false);
     }
 
     array(buffer_t<V> data, const shape_t& shape, const size_t offset, const size_t row_stride, const size_t col_stride,
           const void* base, const bool is_matrix, const bool is_scalar, const bool is_assignable) noexcept :
-        data(std::move(data)), row(shape.rows), col(shape.cols), offset(offset),
+        buffer(std::move(data)), row(shape.rows), col(shape.cols), offset(offset),
         row_stride(row_stride == none::size ? shape.rows : row_stride), col_stride(col_stride), _base(base),
         is_matrix(is_matrix), is_scalar(is_scalar), is_assignable(is_assignable) {}
 
     bool is_contiguous() const { return row_stride == col && col_stride == 1; }
 
 public:
-    array() = delete;
+    array() = default;
 
     array(const array& other) :
-        array(other.data, {other.row, other.col}, other.offset, other.row_stride, other.col_stride,
+        array(other.buffer, {other.row, other.col}, other.offset, other.row_stride, other.col_stride,
               other._base ? other._base : &other, other.is_matrix, other.is_scalar, false) {}
 
     array(array&& other) noexcept = default;
 
     array(std::initializer_list<V> list, const shape_t& shape = none::shape) {
-        flat_constructor(list.begin(), list.end(), shape);
+        *this = flat_constructor(list.begin(), list.end(), shape);
     }
 
     array(const std::vector<V>& list, const shape_t& shape = none::shape) {
-        flat_constructor(list.begin(), list.end(), shape);
+        *this = flat_constructor(list.begin(), list.end(), shape);
     }
 
     array(std::vector<V>&& list, const shape_t& shape = none::shape) :
@@ -65,8 +60,8 @@ public:
         if (row * col != list.size()) {
             throw std::invalid_argument("Size mismatch in flat move constructor");
         }
-        data = buffer_t<V>(std::move(list));
-        std::move(list.begin(), list.end(), data.data());
+        buffer = buffer_t<V>(std::move(list));
+        std::move(list.begin(), list.end(), buffer.data());
     }
 
     array(const std::initializer_list<std::initializer_list<V>>& lists) :
@@ -77,11 +72,11 @@ public:
                 throw std::invalid_argument("Inconsistent list sizes");
             }
         }
-        data = buffer_t<V>(row * col);
+        buffer = buffer_t<V>(row * col);
         size_t index = 0;
 
         for (const std::initializer_list<V>& list : lists) {
-            std::copy(list.begin(), list.end(), data.data() + index);
+            std::copy(list.begin(), list.end(), buffer.data() + index);
             index += col;
         }
     }
@@ -95,13 +90,13 @@ public:
             }
         }
         if (row * col == 0) {
-            data.reset();
+            buffer.reset();
         } else {
-            data = buffer_t<V>(row * col);
+            buffer = buffer_t<V>(row * col);
             size_t index = 0;
 
             for (std::vector<V>& list : lists) {
-                std::move(list.begin(), list.end(), data.data() + index);
+                std::move(list.begin(), list.end(), buffer.data() + index);
                 index += col;
             }
         }
@@ -111,32 +106,51 @@ public:
         row(shape.rows), col(shape.cols), offset(0), row_stride(col), col_stride(1), _base(none::base),
         is_matrix(row > 1 && col > 1), is_scalar(false), is_assignable(false) {
         if (copy) {
-            data = buffer_t<V>(row * col);
-            std::copy(buf.data(), buf.data() + row * col, data.data());
+            buffer = buffer_t<V>(row * col);
+            std::copy_n(buf.data(), row * col, buffer.data());
         } else {
-            data = buf;
+            buffer = buf;
         }
     }
+
+    array(buffer_t<V>&& buf, const shape_t& shape) : array(buf, shape, false) {}
 
     array(V* list, const shape_t& shape, const bool copy = true) :
         row(shape.rows), col(shape.cols), offset(0), row_stride(col), col_stride(1), _base(none::base),
         is_matrix(row > 1 && col > 1), is_scalar(false), is_assignable(false) {
         if (copy) {
-            data = buffer_t<V>(row * col);
-            std::copy(list, list + row * col, data.data());
+            buffer = buffer_t<V>(row * col);
+            std::copy_n(list, row * col, buffer.data());
         } else {
-            data = buffer_t<V>(list, row * col);
+            buffer = buffer_t<V>(list, row * col);
         }
     }
 
     const array* base() const noexcept { return _base; }
+
+    const buffer_t<V>& data() const { return buffer; }
+
+    buffer_t<V>& data() { return buffer; }
+
     size_t ndim() const noexcept { return row > 1 && col > 1 ? 2 : 1; }
+
     size_t size() const noexcept { return row * col; }
 
-    template <class dtype = V>
-    array<dtype> abs(out_t<dtype> out = none::out, const where_t& where = none::where) const;
+    template <typename dtype = V>
+    array<real_t<dtype>> abs(out_t<real_t<dtype>> out = none::out, const where_t& where = none::where) const;
+
+    template <typename dtype = V>
+    array<real_t<dtype>> abs(const where_t& where) const {
+        return abs<real_t<dtype>>(none::out, where);
+    }
+
     auto real(out_t<real_t<V>> = none::out, const where_t& = none::where);
+
+    auto real(const where_t& where) const { return real(none::out, where); }
+
     auto imag(out_t<real_t<V>> = none::out, const where_t& = none::where);
+
+    auto imag(const where_t& where) const { return imag(none::out, where); }
 
     shape_t shape() const noexcept { return {row, col}; }
 
@@ -161,8 +175,8 @@ public:
                     (*this)[{i, j}] = other[broadcast_index({i, j}, rhs_shape)];
                 }
             }
-        } else if (this == &other) {
-            data = other.data;
+        } else if (this != &other) {
+            buffer = other.buffer;
             row = other.row;
             col = other.col;
             offset = other.offset;
@@ -185,7 +199,7 @@ public:
                 }
             }
         } else if (is_scalar) {
-            data[offset] = other;
+            buffer[offset] = other;
         } else {
             throw std::invalid_argument("Illegal assignment of a scalar to a non-scalar array.");
         }
@@ -195,7 +209,7 @@ public:
     friend std::ostream& operator<<(std::ostream& out, const array& other) {
         auto [row, col] = other.shape();
         const bool is_col_vector = (col == 1);
-        const size_t width_dim = is_col_vector ? col : row;
+        const size_t width_dim = is_col_vector ? row : col;
         size_t col_width = 0;
         std::vector<size_t> col_width_vec;
 
@@ -204,9 +218,12 @@ public:
         }
         for (ll_t i = 0; i < row; i++) {
             for (ll_t j = 0; j < col; j++) {
-                const array element = other[{i, j}];
-                const size_t size = format(element.data[element.offset]).size();
-
+                size_t size;
+                if constexpr (std::is_same_v<V, dtype::bool_t>) {
+                    size = detail::format(dtype::bitref_t(other[{i, j}])).size();
+                } else {
+                    size = detail::format(static_cast<V>(other[{i, j}])).size();
+                }
                 if (other.is_matrix) {
                     const size_t pos = is_col_vector ? i : j;
                     col_width_vec[pos] = std::max(col_width_vec[pos], size);
@@ -232,8 +249,11 @@ public:
                 } else {
                     out << std::setw(col_width);
                 }
-                const array element = other[{i, j}];
-                out << format(element.data[element.offset]);
+                if constexpr (std::is_same_v<V, dtype::bool_t>) {
+                    out << detail::format(dtype::bitref_t(other[{i, j}]));
+                } else {
+                    out << detail::format(static_cast<V>(other[{i, j}]));
+                }
             }
             if (other.is_matrix) {
                 out << "]";
@@ -254,12 +274,31 @@ public:
         if (!is_scalar) {
             throw std::invalid_argument("illegal scalar conversion of an array");
         }
-        return data[offset];
+        return buffer[offset];
     }
 
     operator V&() { return const_cast<V&>(static_cast<const array&>(*this).operator const V&()); }
 
-    array T() const { return array(data, {col, row}, offset, col_stride, row_stride, _base ? _base : this, is_matrix); }
+    operator dtype::bitref_t() requires(std::is_same_v<V, dtype::bool_t>)
+    {
+        if (!is_scalar) {
+            throw std::invalid_argument("illegal scalar conversion of an array");
+        }
+        return dtype::bitref_t(buffer[offset / 8].value, offset % 8);
+    }
+
+
+    operator dtype::bool_t() const requires(std::is_same_v<V, dtype::bool_t>)
+    {
+        if (!is_scalar) {
+            throw std::invalid_argument("illegal scalar conversion of an array");
+        }
+        return dtype::bool_t((buffer[offset / 8].value >> (offset % 8)) & 1);
+    }
+
+    // array T() const {
+    //     return array(buffer, {col, row}, offset, col_stride, row_stride, _base ? _base : this, is_matrix);
+    // }
 
     array reshape(const size_t rows, const size_t cols) const {
         if (rows * cols != size()) {
@@ -268,12 +307,12 @@ public:
         if (!is_contiguous()) {
             throw std::runtime_error("cannot reshape a non-contiguous array");
         }
-        return array(data, {rows, cols}, offset, cols, 1, _base ? _base : this, rows > 1 && cols > 1);
+        return array(buffer, {rows, cols}, offset, cols, 1, _base ? _base : this, rows > 1 && cols > 1);
     }
 
     array copy() const {
         buffer_t<V> buf = buffer_t<V>(row * col);
-        std::copy(data.data(), data.data() + row * col, buf.data());
-        return array(std::move(buf), {row, col}, offset, row_stride, col_stride, _base, is_matrix, is_scalar, false);
+        std::copy_n(buffer.data(), row * col, buf.data());
+        return array(std::move(buf), {row, col}, offset, row_stride, col_stride, nullptr, is_matrix, is_scalar, false);
     }
 };
