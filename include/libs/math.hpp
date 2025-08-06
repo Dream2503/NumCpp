24a87d3d8cfee218926cdcc1da791331b6555ace
Dream2503 <swapnarajmohanty2503.com>
2025-08-07 01:25:52 +0530

    added the real_type trait for consistent output type for complex as well as real types

--- include/libs/math.hpp
+++ include/libs/math.hpp
@@ -4,7 +4,7 @@ namespace numcpp::math {
     template <typename V, typename dtype = V>
     auto abs(const V value) {
         if constexpr (is_complex_v<V>) {
-            return static_cast<dtype>(value.abs());
+            return static_cast<real_t<dtype>>(value.abs());
         } else {
             return static_cast<dtype>(std::abs(value));
         }
