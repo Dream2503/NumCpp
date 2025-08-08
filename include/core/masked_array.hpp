c70221682a0786a5e5a51858e694dcfb6f3b67a0
Dream2503 <swapnarajmohanty2503.com>
2025-08-09 01:07:34 +0530

    -> added operator= and bool() with some minor tweaks from array -> minor changes

--- include/core/masked_array.hpp
+++ include/core/masked_array.hpp
@@ -1,15 +1,14 @@
 #pragma once
-#include "../libs/utils.hpp"
 
 namespace numcpp {
     class MaskedArray : public array<dtype::bool_t> {
-        MaskedArray(auto begin, auto end, shape_t shape) : array() {
+        MaskedArray(auto begin, auto end, shape_t shape) {
             const size_t size = end - begin;
 
             if (shape.cols == none::size) {
                 shape.cols = size;
             }
-            if (shape.rows * shape.cols != size) {
+            if (shape.size() != size) {
                 throw std::invalid_argument("Size mismatch in flat constructor");
             }
             buffer_t<dtype::bool_t> buf((size + 7) / 8);
@@ -23,17 +22,17 @@ namespace numcpp {
             *this = array(std::move(buf), shape);
         }
 
-        MaskedArray(const array& other) : array(other) {}
-        MaskedArray(array&& other) noexcept : array(other) {}
-
     public:
         using array::array;
         using array::operator=;
 
+        MaskedArray(const array& other) : array(other) {}
+        MaskedArray(array&& other) noexcept : array(other) {}
+
         MaskedArray(const std::initializer_list<bool> list, const shape_t& shape = none::shape) :
             MaskedArray(list.begin(), list.end(), shape) {}
 
-        MaskedArray(const std::initializer_list<std::initializer_list<bool>> lists) : array() {
+        MaskedArray(const std::initializer_list<std::initializer_list<bool>> lists) {
             size_t rows = lists.size();
             size_t cols = lists.begin()->size();
             const size_t size = rows * cols;
@@ -65,42 +64,27 @@ namespace numcpp {
 
         MaskedArray operator[](const index_t& index) const { return MaskedArray(array::operator[](index)); }
 
-        MaskedArray operator~() const {
-            const size_t size = (row * col + 7) / 8;
-            MaskedArray result = this->copy();
-
-            for (size_t i = 0; i < size; i++) {
-                result.buffer[i].value = ~result.data()[i].value;
+        MaskedArray& operator=(const bool& other) {
+            if (is_scalar) {
+                dtype::bitref_t(buffer[offset / 8].value, offset % 8) = other;
+            } else if (is_assignable) {
+                for (ll_t i = 0; i < row; i++) {
+                    for (ll_t j = 0; j < col; j++) {
+                        (*this)[{i, j}] = other;
+                    }
+                }
+            } else {
+                throw std::invalid_argument("Illegal assignment of a scalar to a non-scalar array.");
             }
-            return result;
-        }
-
-        MaskedArray operator&(const MaskedArray& other) const {
-            return binary_opr_broadcast(*this, other, std::bit_and(), std::false_type{});
-        }
-
-        MaskedArray operator|(const MaskedArray& other) const {
-            return binary_opr_broadcast(*this, other, std::bit_or(), std::false_type{});
-        }
-
-        MaskedArray operator^(const MaskedArray& other) const {
-            return binary_opr_broadcast(*this, other, std::bit_xor(), std::false_type{});
-        }
-
-        MaskedArray& operator&=(const MaskedArray& other) {
-            binary_opr_broadcast(*this, other, std::bit_and(), std::true_type{});
+            is_assignable = false;
             return *this;
         }
 
-        MaskedArray& operator|=(const MaskedArray& other) {
-            binary_opr_broadcast(*this, other, std::bit_or(), std::true_type{});
-            return *this;
-        }
-
-        MaskedArray& operator^=(const MaskedArray& other) {
-            binary_opr_broadcast(*this, other, std::bit_xor(), std::true_type{});
-            return *this;
+        operator const bool() const {
+            if (!is_scalar) {
+                throw std::invalid_argument("illegal boolean conversion of an MaskedArray");
+            }
+            return dtype::bitref_t(*this);
         }
     };
-
 } // namespace numcpp
