c9832b35ce3936f3329e9ed6460702d1b8882467
Dream2503 <swapnarajmohanty2503.com>
2025-08-09 01:06:18 +0530

    -> added compile time constrain on abs function to narrow down its scope -> added floor()

--- include/libs/math.hpp
+++ include/libs/math.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 namespace numcpp::math {
-    template <typename V, typename dtype = V>
-    auto abs(const V value) {
-        if constexpr (is_complex_v<V>) {
+    template <typename T, typename dtype = T>
+    requires(is_numeric_v<T>)
+    auto abs(const T value) {
+        if constexpr (is_complex_v<T>) {
             return static_cast<real_t<dtype>>(value.abs());
         } else {
             return static_cast<dtype>(std::abs(value));
         }
+    }
+
+    template <typename T, typename dtype = T>
+    requires(is_numeric_v<T> && !is_complex_v<T>)
+    auto floor(const T value) {
+        return static_cast<dtype>(std::floor(value));
     }
 } // namespace numcpp::math
