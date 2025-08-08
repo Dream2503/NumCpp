fffa21e0b89eb5a0a33f59f7d76917d042a26076
Dream2503 <swapnarajmohanty2503.com>
2025-08-09 00:55:17 +0530

    -> refactored template typename from 'V' to 'T' -> added friend func to access private members of array

--- include/libs/utils.hpp
+++ include/libs/utils.hpp
@@ -1,36 +1,55 @@
 #pragma once
-#include "traits.hpp"
 
 namespace numcpp {
-    template <typename V>
-    array<V> arange(const range_t<V>& range) {
-        return array(std::vector<V>(range.begin(), range.end()), {1, range.size()});
+    template <typename T>
+    array<T> arange(const range_t<T>& range) {
+        return array(range.evaluate(), {1, range.size()});
     }
 
-    template <typename V>
-    array<V> fill(const shape_t& shape, const V& value) {
-        const size_t size = shape.rows * shape.cols;
-        buffer_t<V> buf(size);
+    template <typename T>
+    array<T> fill(const shape_t& shape, const T& value) {
+        const size_t size = shape.size();
+        buffer_t<T> buf(size);
         std::fill_n(buf.data(), size, value);
-        return array<V>(buf, shape, false);
+        return array<T>(std::move(buf), shape);
     }
 
-    template <typename V = double>
-    array<V> ones(const shape_t& shape) {
-        return fill(shape, V(1));
+    template <typename T = double>
+    array<T> ones(const shape_t& shape) {
+        return fill(shape, T(1));
     }
 
-    template <typename V>
-    array<V> empty(const shape_t& shape) {
-        if (shape.rows * shape.cols) {
-            buffer_t<V> data(shape.rows * shape.cols);
-            return array<V>(data, shape, false);
+    template <typename T>
+    array<T> empty(const shape_t& shape) {
+        if (shape.size()) {
+            return array<T>(buffer_t<T>(shape.size()), shape);
         }
-        return array<V>();
+        return array<T>();
     }
 
-    template <typename V = double>
-    array<V> zeros(const shape_t& shape) {
-        return fill(shape, V());
+    template <typename T = double>
+    array<T> zeros(const shape_t& shape) {
+        return fill(shape, T());
+    }
+
+    template <typename T>
+    size_t offset(const array<T>& arr) noexcept {
+        return arr.offset;
+    }
+    template <typename T>
+    buffer_t<T> buffer(const array<T>& arr) noexcept {
+        return arr.buffer;
+    }
+    template <typename T>
+    bool is_matrix(const array<T>& arr) noexcept {
+        return arr.is_matrix;
+    }
+    template <typename T>
+    bool is_scalar(const array<T>& arr) noexcept {
+        return arr.is_scalar;
+    }
+    template <typename T>
+    bool is_assignable(const array<T>& arr) noexcept {
+        return arr.is_assignable;
     }
 } // namespace numcpp
